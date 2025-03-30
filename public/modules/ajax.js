233fd91a220d8a4862001b936a931fbcae023c0d
Your Name <you@example.com>
2025-03-30 21:37:59 +0300

    messenger layout

--- public/modules/ajax.js
+++ public/modules/ajax.js
@@ -4,7 +4,7 @@ import { users, chats, messages } from '../mocks.js'
 const HTTP_METHOD_GET = 'GET';
 const HTTP_METHOD_POST = 'POST';
 const API_BASE_URL = 'https://quickflowapp.ru/api';
-const DEVELOP = true;
+const DEVELOP = false;
 
 
 class Ajax {
