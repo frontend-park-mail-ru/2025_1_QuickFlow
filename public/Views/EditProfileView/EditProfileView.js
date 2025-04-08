11242b1041cd47a2432bd438f17fc53bdb885be7
Your Name <you@example.com>
2025-04-08 23:23:25 +0300

    edit profile

--- public/Views/EditProfileView/EditProfileView.js
+++ public/Views/EditProfileView/EditProfileView.js
@@ -279,6 +279,8 @@ export default class EditProfileView {
             const sections = {
                 profile: () => {
                     body.profile ??= {};
+                    console.log(name);
+                    console.log(value);
                     if (name === 'avatar_url' || name === 'cover_url') {
                         body[name] = value.length === 1 ? value : '';
                         return;
@@ -316,15 +318,12 @@ export default class EditProfileView {
         //     }
         // }
 
-        console.log("before condition", body);
-
         if (!body['cover_url']) body['cover_url'] = '';
         if (!body['avatar_url']) body['avatar_url'] = '';
 
         console.log(body);
 
         const fd = convertToFormData(body);
-        console.log(fd);
 
         Ajax.post({
             url: '/profile',
