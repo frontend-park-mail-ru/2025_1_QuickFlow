212d7c77ad7b827f193e1f06fb64edc9aae83f18
Your Name <you@example.com>
2025-04-14 09:07:53 +0300

    time format

--- public/Components/MessengerComponent/ChatWindowComponent.js
+++ public/Components/MessengerComponent/ChatWindowComponent.js
@@ -122,12 +122,10 @@ export default class ChatWindowComponent {
                             this.renderChat();
                             ws.subscribe('message', (payload) => {
                                 console.log(payload);
-                                if (!this.#chatData?.id && this.#chatData?.receiver_id) {
-                                    setLsItem('active-chat', `chat-${payload.chat_id}`);
-                                    this.#chatsPanel.renderChatList();
-                                }
-                                this.#msgs.push(payload);
-                                this.#chat.renderMsg(payload, []);
+                                setLsItem('active-chat', `chat-${payload.chat_id}`);
+                                this.#chatsPanel.renderChatList();
+                                // this.#msgs.push(payload);
+                                // this.#chat.renderMsg(payload, []);
                             });
                             this.renderMessageInput();
 
