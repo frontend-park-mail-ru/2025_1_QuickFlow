06c7fc426748e2bedb1deaafb0363df127ac2f37
Your Name <you@example.com>
2025-05-06 19:29:28 +0400

    community

--- public/sw.ts
+++ public/sw.ts
@@ -1,4 +1,4 @@
-const VERSION = '1.0.4'; // Версия для управления кэшами
+const VERSION = '1.0.5'; // Версия для управления кэшами
 const STATIC_CACHE = 'STATIC_CACHE-' + VERSION;
 const MEDIA_CACHE = 'MEDIA_CACHE-' + VERSION;
 const API_CACHE = 'API_CACHE-' + VERSION;
