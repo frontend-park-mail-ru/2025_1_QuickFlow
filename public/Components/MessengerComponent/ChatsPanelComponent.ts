2d7f90d1696078622289a1a22038033a0a9c6b78
Your Name <you@example.com>
2025-05-07 17:09:25 +0400

    community

--- public/Components/MessengerComponent/ChatsPanelComponent.ts
+++ public/Components/MessengerComponent/ChatsPanelComponent.ts
@@ -111,6 +111,7 @@ export default class ChatsPanelComponent {
                     this.activeChatItem?.classList.add('chats-panel__chat_active');
                     this._chatWindow.renderActiveChat(chatData);
                     if (firstMsgSent) {
+                        console.log('firstMsgSent');
                         var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + `chat_id=${this._chatWindow.msgsData.messages[0].chat_id}`;
                         window.history.pushState({path: newurl}, '', newurl);
                     }
