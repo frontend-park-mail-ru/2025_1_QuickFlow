a35a425246c477bdebe19eb7288c66188de869e2
Your Name <you@example.com>
2025-05-24 21:38:00 +0300

    post response reformat

--- public/utils/convertToFormData.ts
+++ public/utils/convertToFormData.ts
@@ -5,6 +5,10 @@ export default function convertToFormData(obj: object, fd: FormData = new FormDa
         const fieldName = prevKey ? `${prevKey}[${key}]` : key;
         if (value && typeof value === 'object' && !(value instanceof File || value instanceof FileList || value instanceof Date)) {
             convertToFormData(value, fd, fieldName);
+        } else if (value instanceof FileList) {
+            for (const file of value) {
+                fd.append(fieldName, file);
+            }
         } else {
             fd.append(fieldName, value ?? '');
         }
