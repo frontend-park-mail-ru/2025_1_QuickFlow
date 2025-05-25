00275a1ee7e15ee2b72aa905010849fac6cfa43c
Your Name <you@example.com>
2025-05-25 10:20:21 +0300

    attachments responses reformat

--- public/sw.ts
+++ public/sw.ts
@@ -1,4 +1,4 @@
-const VERSION = '1.0.91'; // Версия для управления кэшами
+const VERSION = '1.0.92'; // Версия для управления кэшами
 
 const STATIC_CACHE = 'STATIC_CACHE-' + VERSION;
 const MEDIA_CACHE = 'MEDIA_CACHE-' + VERSION;
